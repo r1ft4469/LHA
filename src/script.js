7621a426cb173e3c1ce8da6e097a1664ecfb3607
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-04-12 15:06:27 -0400

    write script iteration 1
    writing this at school so you can bet I messed something up lmao

--- src/script.js
+++ src/script.js
@@ -1,1 +1,28 @@
+"use strict";
 
+const database = DatabaseSever.tables
+const alert = new Audio("../db/Audio/alert.mp3")
+
+class LHA () {
+  constructor () {
+    path = ModLoader.getModPath["kobra-LHA"]
+    ModLoader.onModLoad[path] = path.load.bind(path)
+  }
+  
+  load() {
+  
+  }
+  
+  static doTheThing(sessionID) {
+    let pmcData = ProfileController.getPmcProfile(sessionID)
+    
+    if (pmcData.Health.Overall <= 220) {
+      alert.play()
+      alert.loop() = true
+    } else {
+      alert.stop() 
+    }
+  }
+}
+
+module.exports.Mod = LHA
